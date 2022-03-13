c318c3eb56b1146d87f62c4c2e3bcc9314581dfa
JoaoTaranto <tarantoj04@gmail.com>
2022-03-13 19:48:28 -0300

    Atualização Geral JS

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+var valorConvertido = document.getElementById("valorConvertido")
+
 function Converter() {
-    var valor = document.getElementById("valor").value;
-    var valorDolar = parseFloat(valor);
-    var valorReal = valorDolar * 5;
-    locValorConvertido = document.getElementById("valorConvertido");
-    resultadoConvertido = "O resultado da conversão foi de R$" + valorReal;
-    locValorConvertido.innerHTML = resultadoConvertido;
+    var valorEmReal = document.getElementById("valor").value;
+    var valorReal = parseFloat(valorEmReal);
+    var dolar = valorReal * 0.20;
+    var euro = valorReal * 0.18;
+    var btc = valorReal * 0.0000051;
+    
+    var listaMoedas = document.getElementById("menuMoedas");
+    var moeda = listaMoedas.options[listaMoedas.selectedIndex].value;
+
+    if (moeda == "nan") {
+      valorConvertido.innerHTML = ("Escolha uma moeda para conversão.");
+    }
+    if (moeda == "dolar" ) {
+    valorConvertido.innerHTML = ("Olá, o valor de sua conversão foi de U$" + dolar);
+  } else if (moeda == "euro") {
+    valorConvertido.innerHTML = ("Olá, o valor de sua conversão foi de €" + euro);
+  } else if (moeda == "btc") {
+    valorConvertido.innerHTML = ("Olá, o valor de sua conversão foi de BTC " + btc);
   }
+}
